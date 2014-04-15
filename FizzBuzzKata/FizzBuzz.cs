2c825d78db6a19e7dc583b21625cbb29b1a02d4a
Wayne Reid <wreid@zaptechnology.com>
2014-04-15 23:59:35 +1000

    Initial FizzBuzz

---
+++ FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzzKata
+{
+    public class FizzBuzz
+    {
+        public string Request(int number)
+        {
+            string returnVal = string.Empty;
+
+            if (number%3 == 0)
+                returnVal += "fizz";
+
+            if (number%5 == 0)
+                returnVal += "buzz";
+
+            return returnVal;
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,50 @@
+﻿using FizzBuzzKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyAssertions;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void Request_NotDivisible_NullString()
+        {
+            var sut = new FizzBuzz();
+
+            string result = sut.Request(7);
+
+            result.ShouldBe(string.Empty);
+        }
+
+        [TestMethod]
+        public void Request_DivisibleBy3_Fizz()
+        {
+            var sut = new FizzBuzz();
+
+            string result = sut.Request(3);
+
+            result.ShouldBe("fizz");
+        }
+
+        [TestMethod]
+        public void Request_DivisibleBy5_Buzz()
+        {
+            var sut = new FizzBuzz();
+
+            string result = sut.Request(5);
+
+            result.ShouldBe("buzz");
+        }
+
+        [TestMethod]
+        public void Request_DivisibleBy3And5_FizzBuzz()
+        {
+            var sut = new FizzBuzz();
+
+            string result = sut.Request(15);
+
+            result.ShouldBe("fizzbuzz");
+        }
+    }
+}

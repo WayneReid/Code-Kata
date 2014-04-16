1cadef8ad1144af03e2ddc9e5bf349e2f80788bd
Wayne Reid <wreid@zaptechnology.com>
2014-04-16 14:56:18 +1000

    Prime factors kata completed

---
+++ PrimeFactors/PrimeFactorizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class PrimeFactorizer
+    {
+        public IList<int> Factorize(int nunber)
+        {
+            var primes = new List<int>();
+
+            int candidate = 2;
+            while (nunber > 1)
+            {
+                while (nunber % candidate == 0)
+                {
+                    primes.Add(candidate);
+                    nunber /= candidate;
+                }
+                candidate++;
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ PrimeFactorsTests/PrimeFactorizerTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using EasyAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeFactors;
+
+namespace PrimeFactorsTests
+{
+    [TestClass]
+    public class PrimeFactorizerTests
+    {
+        [TestMethod]
+        public void Generate_PrimesOf1_EmptyList()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(1);
+
+            result.ShouldBeEmpty();
+        }
+
+        [TestMethod]
+        public void Generate_PrimesOf2_Returned2()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(2);
+
+            result.ShouldMatch(2);
+        }
+
+        [TestMethod]
+        public void Generate_PrimesOf3_Returned3()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(3);
+
+            result.ShouldMatch(3);
+        }
+
+        [TestMethod]
+        public void Generate_PrimesOf6_Returned3x2()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(6);
+
+            result.ShouldContainItems(new[] {3, 2});
+        }
+
+        [TestMethod]
+        public void Generate_PrimesOf8_Returned2x2x2()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(8);
+
+            result.ShouldMatch(2, 2, 2);
+        }
+
+        [TestMethod]
+        public void Generate_PrimesOf9_Returned3x3()
+        {
+            var sut = new PrimeFactorizer();
+
+            IList<int> result = sut.Factorize(9);
+
+            result.ShouldMatch(3, 3);
+        }
+    }
+}

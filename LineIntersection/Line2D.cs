77fd59d962cd1315cb83e63a479fb5e7ce398f1b
Wayne Reid <wreid@zaptechnology.com>
2014-04-24 16:51:40 +1000

    LineIntersection kata

---
+++ LineIntersection/Line2D.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineIntersection
+{
+    public class Line2D
+    {
+        private readonly Point _point1;
+        private readonly Point _point2;
+
+        public Line2D(Point point1, Point point2)
+        {
+            if (point1 == point2)
+                throw new ArgumentException();
+
+
+            _point1 = point1;
+            _point2 = point2;
+        }
+
+        public bool IntersectsWith(Line2D otherLine)
+        {
+            return Slope() != otherLine.Slope();
+        }
+
+        public decimal Slope()
+        {
+            return (_point2.Y - _point1.Y)/(_point2.X - _point1.X);
+        }
+    }
+
+    public struct Point
+    {
+        public Point(decimal x, decimal y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+
+        #region Equality
+
+        public bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Point && Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X.GetHashCode()*397) ^ Y.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+}
---
+++ LineIntersectionTests/Line2DTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using LineIntersection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyAssertions;
+
+namespace LineIntersectionTests
+{
+    [TestClass]
+    public class Line2DTests
+    {
+        [TestMethod]
+        public void IntersectsWith_IdenticalPointsInConstructor_InvalidArgumentException()
+        {
+            bool didThrow = false;
+
+            try
+            {
+                Line2D sut = new Line2D(new Point(0, 0), new Point(0, 0));
+            }
+            catch (ArgumentException)
+            {
+                didThrow = true;                
+            }
+
+            didThrow.ShouldBe(true);
+        }
+        
+        
+        [TestMethod]
+        public void IntersectsWith_ParrallelLines_ReturnsFalse()
+        {
+            Line2D line1 = new Line2D(new Point(0, 0), new Point(1, 1));
+            Line2D line2 = new Line2D(new Point(0, 1), new Point(1, 2));
+
+            bool result = line1.IntersectsWith(line2);
+
+            result.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void IntersectsWith_IntersectingLines_ReturnsTrue()
+        {
+            Line2D line1 = new Line2D(new Point(0, 0), new Point(1, 1));
+            Line2D line2 = new Line2D(new Point(0, 4), new Point(1, 0));
+
+            bool result = line1.IntersectsWith(line2);
+
+            result.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Slope_FlatSlope_Returns0()
+        {
+            Line2D sut = new Line2D(new Point(0, 0), new Point(4, 0));
+
+            decimal result = sut.Slope();
+
+            result.ShouldBe((decimal)0);
+        }
+
+        [TestMethod]
+        public void Slope_AtypicalSlope_ReturnsSlope()
+        {
+            Line2D sut = new Line2D(new Point(0, 0), new Point(4, 4));
+
+            decimal result = sut.Slope();
+
+            result.ShouldBe((decimal)1);
+        }
+
+        [TestMethod]
+        public void Slope_InfiniteSlope_ReturnsSlope()
+        {
+            Line2D sut = new Line2D(new Point(0, 0), new Point(0, 4));
+
+            bool thrown = false;
+            try
+            {
+                sut.Slope();
+            }
+            catch (DivideByZeroException)
+            {
+                thrown = true;
+            }
+
+            thrown.ShouldBe(true);
+        }
+    }
+}

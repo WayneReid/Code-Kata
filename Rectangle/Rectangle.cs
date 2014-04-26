81cba97490012365165f2d15566299739cd8c5f8
Wayne Reid <wreid@zaptechnology.com>
2014-04-26 15:59:04 +1000

    Intersecting rectangles kata

---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RectangleIntersection
+{
+    public class Rectangle
+    {
+        private readonly double _xMin;
+        private readonly double _xMax;
+        private readonly double _yMin;
+        private readonly double _yMax;
+
+        public Rectangle(double xMin, double xMax, double yMin, double yMax)
+        {
+            if (xMin >= xMax)
+                throw new ArgumentException("xMin >= xMax");
+
+            if (yMin >= yMax)
+                throw new ArgumentException("yMin >= yMax");
+
+            _xMin = xMin;
+            _xMax = xMax;
+            _yMin = yMin;
+            _yMax = yMax;
+        }
+
+        public double Area()
+        {
+            return Length*Width;
+        }
+
+        private double Length
+        {
+            get { return _xMax - _xMin; }
+        }
+
+        private double Width
+        {
+            get { return _yMax - _yMin; }
+        }
+
+        public double Circumference()
+        {
+            return 2*Width + 2*Length;
+        }
+
+        public bool OverlapsWith(Rectangle overlappingRectangle)
+        {
+            if (overlappingRectangle._xMax < _xMin)
+                return false;
+
+            if (overlappingRectangle._xMin > _xMax)
+                return false;
+
+            if (overlappingRectangle._yMax < _yMin)
+                return false;
+
+            if (overlappingRectangle._yMin > _yMax)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ RectangleTests/RectangleTests.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RectangleIntersection;
+using EasyAssertions;
+
+namespace RectangleTests
+{
+    [TestClass]
+    public class RectangleTests
+    {
+        [TestMethod]
+        public void Constructor_xMinGreaterThanOrEqualToxMax_ArgumentException()
+        {           
+            bool thrown = false;
+
+            try
+            {
+                new Rectangle(5, 3, 0, 1);
+            }
+            catch (ArgumentException)
+            {
+                thrown = true;
+            }
+            thrown.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Constructor_yMinGreaterThanOrEqualToyMax_ArgumentException()
+        {
+            bool thrown = false;
+
+            try
+            {
+                new Rectangle(0, 1, 5, 4);
+            }
+            catch (ArgumentException)
+            {
+                thrown = true;
+            }
+            thrown.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Area_Sides4_Returns16()
+        {
+            var sut = new Rectangle(0, 4, 0, 4);
+
+            double result = sut.Area();
+
+            result.ShouldBe(16.0, 0.0);
+        }
+
+        [TestMethod]
+        public void Area_Sides2x4_Returns8()
+        {
+            var sut = new Rectangle(0, 2, 0, 4);
+
+            double result = sut.Area();
+
+            result.ShouldBe(8.0, 0.0);
+        }
+
+        [TestMethod]
+        public void Circumference_Sides4_Returns16()
+        {
+            var sut = new Rectangle(0, 4, 0, 4);
+
+            double result = sut.Circumference();
+
+            result.ShouldBe(16.0, 0.0);
+        }
+
+        [TestMethod]
+        public void Circumference_Sides2x4_Returns12()
+        {
+            var sut = new Rectangle(0, 2, 0, 4);
+
+            double result = sut.Circumference();
+
+            result.ShouldBe(12.0, 0.0);
+        }
+
+        [TestMethod]
+        public void Overlaps_OverlappingRectangles_ReturnsTrue()
+        {
+            var sut = new Rectangle(0, 2, 0, 4);
+            var overlappingRectangle = new Rectangle(0, 4, 0, 4);
+
+            bool result = sut.OverlapsWith(overlappingRectangle);
+
+            result.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Overlaps_EdgesOverlappingOnly_ReturnsTrue()
+        {
+            var sut = new Rectangle(0, 2, 0, 4);
+            var overlappingRectangle = new Rectangle(2, 4, 0, 4);
+
+            bool result = sut.OverlapsWith(overlappingRectangle);
+
+            result.ShouldBe(true);
+        }
+    }
+}
